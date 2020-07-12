c5d3969ad331720e53a2735ef05b70977dd465d0
Vadim Kaushan <admin@disasm.info>
2020-07-12 17:25:26 +0300

    USB setup code

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -12,6 +12,13 @@ use rtt_target::{rtt_init_print, rprintln};
 fn main() -> ! {
     rtt_init_print!();
 
+    let rcc = hs_probe_bsp::rcc::RCC::new(stm32ral::rcc::RCC::take().unwrap());
+
+    let usb_global = stm32ral::otg_fs_global::OTG_FS_GLOBAL::take().unwrap();
+    let usb_device = stm32ral::otg_fs_device::OTG_FS_DEVICE::take().unwrap();
+    let usb_pwrclk = stm32ral::otg_fs_pwrclk::OTG_FS_PWRCLK::take().unwrap();
+    let mut usb = crate::usb::USB::new(usb_global, usb_device, usb_pwrclk);
+
     rprintln!("Starting...");
 
     loop {
--- firmware/src/usb.rs
+++ firmware/src/usb.rs
@@ -3,13 +3,37 @@ use stm32ral::{
     otg_fs_device,
     otg_fs_pwrclk
 };
-
 use crate::app::Request;
 use hs_probe_bsp::rcc::Clocks;
+use hs_probe_bsp::otg_fs::{UsbBusType, UsbBus};
+use usb_device::prelude::*;
+use usb_device::bus::UsbBusAllocator;
+use usbd_serial::SerialPort;
 
+
+struct UninitializedUSB {
+    global: otg_fs_global::Instance,
+    device: otg_fs_device::Instance,
+    pwrclk: otg_fs_pwrclk::Instance,
+}
+
+struct InitializedUSB {
+    device: UsbDevice<'static, UsbBusType>,
+    serial: SerialPort<'static, UsbBusType>,
+}
+
+enum State {
+    Uninitialized(UninitializedUSB),
+    Initialized(InitializedUSB),
+    Initializing,
+}
+
+static mut EP_MEMORY: [u32; 1024] = [0; 1024];
+static mut USB_BUS: Option<UsbBusAllocator<UsbBusType>> = None;
 
 /// USB stack interface
 pub struct USB {
+    state: State,
 }
 
 impl USB {
@@ -19,12 +43,50 @@ impl USB {
         device: otg_fs_device::Instance,
         pwrclk: otg_fs_pwrclk::Instance,
     ) -> Self {
-        todo!()
+        let usb = UninitializedUSB {
+            global,
+            device,
+            pwrclk
+        };
+        USB {
+            state: State::Uninitialized(usb)
+        }
     }
 
     /// Initialise the USB peripheral ready to start processing packets
     pub fn setup(&mut self, clocks: &Clocks) {
-        todo!()
+        let state = core::mem::replace(&mut self.state, State::Initializing);
+        if let State::Uninitialized(usb) = state {
+            cortex_m::interrupt::free(|_| unsafe {
+                let usb = hs_probe_bsp::otg_fs::USB {
+                    usb_global: usb.global,
+                    usb_device: usb.device,
+                    usb_pwrclk: usb.pwrclk,
+                    hclk: clocks.hclk()
+                };
+
+                let usb_bus = UsbBus::new(usb, &mut EP_MEMORY);
+                USB_BUS = Some(usb_bus);
+                let usb_bus = USB_BUS.as_ref().unwrap();
+
+                let serial = SerialPort::new(&usb_bus);
+
+                let device = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0xFF50))
+                    .manufacturer("AGG")
+                    .product("FFP r1 with CMSIS-DAP Support")
+                    .serial_number(stm32_device_signature::device_id_hex())
+                    .device_class(0)
+                    .build();
+
+                let usb = InitializedUSB {
+                    device,
+                    serial,
+                };
+                self.state = State::Initialized(usb)
+            });
+        } else {
+            panic!("Invalid state");
+        }
     }
 
     /// Process a pending USB interrupt.
@@ -66,7 +128,7 @@ impl USB {
         todo!()
     }
 
-    // Indicate we cannot currently receive DAP requests
+    /// Indicate we cannot currently receive DAP requests
     pub fn dap_disable(&mut self) {
         todo!()
     }
