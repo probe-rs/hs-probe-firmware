57c6c401b59a9cb8672387bad402c645f782b153
Vadim Kaushan <admin@disasm.info>
2020-10-04 01:42:19 +0300

    Switch firmware to USB HS

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -21,10 +21,11 @@ fn main() -> ! {
 
     let rcc = bsp::rcc::RCC::new(stm32ral::rcc::RCC::take().unwrap());
 
-    let usb_global = stm32ral::otg_fs_global::OTG_FS_GLOBAL::take().unwrap();
-    let usb_device = stm32ral::otg_fs_device::OTG_FS_DEVICE::take().unwrap();
-    let usb_pwrclk = stm32ral::otg_fs_pwrclk::OTG_FS_PWRCLK::take().unwrap();
-    let mut usb = crate::usb::USB::new(usb_global, usb_device, usb_pwrclk);
+    let usb_phy = stm32ral::usbphyc::USBPHYC::take().unwrap();
+    let usb_global = stm32ral::otg_hs_global::OTG_HS_GLOBAL::take().unwrap();
+    let usb_device = stm32ral::otg_hs_device::OTG_HS_DEVICE::take().unwrap();
+    let usb_pwrclk = stm32ral::otg_hs_pwrclk::OTG_HS_PWRCLK::take().unwrap();
+    let mut usb = crate::usb::USB::new(usb_phy, usb_global, usb_device, usb_pwrclk);
 
     let dma = bsp::dma::DMA::new(
         stm32ral::dma::DMA1::take().unwrap(),
@@ -57,8 +58,8 @@ fn main() -> ! {
         spi2_clk: gpioi.pin(1),
         spi2_miso: gpioi.pin(2),
         spi2_mosi: gpioi.pin(3),
-        usb_dm: gpioa.pin(11),
-        usb_dp: gpioa.pin(12),
+        usb_dm: gpiob.pin(14),
+        usb_dp: gpiob.pin(15),
     };
 
     let swd = swd::SWD::new(&spi1, &pins);
