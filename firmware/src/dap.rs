030fa398d5716512c44d8bde5aab662895f7aae5
Grant Miller <GrantM11235@gmail.com>
2021-02-27 15:55:15 -0600

    Comment out unsupported commands
    This makes is much easier to see which commands are unsupported

--- firmware/src/dap.rs
+++ firmware/src/dap.rs
@@ -20,6 +20,7 @@ pub enum DAPVersion {
 #[allow(non_camel_case_types)]
 #[repr(u8)]
 enum Command {
+    // General Commands
     DAP_Info = 0x00,
     DAP_HostStatus = 0x01,
     DAP_Connect = 0x02,
@@ -28,13 +29,16 @@ enum Command {
     DAP_Delay = 0x09,
     DAP_ResetTarget = 0x0A,
 
+    // Common SWD/JTAG Commands
     DAP_SWJ_Pins = 0x10,
     DAP_SWJ_Clock = 0x11,
     DAP_SWJ_Sequence = 0x12,
 
+    // SWD Commands
     DAP_SWD_Configure = 0x13,
-    DAP_SWD_Sequence = 0x1D,
+    // DAP_SWD_Sequence = 0x1D,
 
+    // SWO Commands
     DAP_SWO_Transport = 0x17,
     DAP_SWO_Mode = 0x18,
     DAP_SWO_Baudrate = 0x19,
@@ -43,18 +47,22 @@ enum Command {
     DAP_SWO_ExtendedStatus = 0x1E,
     DAP_SWO_Data = 0x1C,
 
+    // JTAG Commands
     DAP_JTAG_Sequence = 0x14,
-    DAP_JTAG_Configure = 0x15,
-    DAP_JTAG_IDCODE = 0x16,
+    // DAP_JTAG_Configure = 0x15,
+    // DAP_JTAG_IDCODE = 0x16,
 
+    // Transfer Commands
     DAP_TransferConfigure = 0x04,
     DAP_Transfer = 0x05,
     DAP_TransferBlock = 0x06,
     DAP_TransferAbort = 0x07,
 
-    DAP_ExecuteCommands = 0x7F,
-    DAP_QueueCommands = 0x7E,
+    // Atomic Commands
+    // DAP_ExecuteCommands = 0x7F,
+    // DAP_QueueCommands = 0x7E,
 
+    // Unimplemented Command Response
     Unimplemented = 0xFF,
 }
 
@@ -297,7 +305,9 @@ impl<'a> DAP<'a> {
             Command::DAP_Transfer => self.process_transfer(req),
             Command::DAP_TransferBlock => self.process_transfer_block(req),
             Command::DAP_TransferAbort => self.process_transfer_abort(req),
-            _ => Some(ResponseWriter::new(Command::Unimplemented, &mut self.rbuf)),
+            Command::Unimplemented => {
+                Some(ResponseWriter::new(Command::Unimplemented, &mut self.rbuf))
+            }
         }
         .map(|resp| resp.finished())
     }
