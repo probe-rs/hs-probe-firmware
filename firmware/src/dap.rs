1c5c882b4588deb70080464a6dcde58922893872
Gabriel Górski <gabriel.gorski@volvocars.com>
2022-01-03 13:08:12 +0100

    Make sure that SWD pins are in a correct mode

--- firmware/src/dap.rs
+++ firmware/src/dap.rs
@@ -564,6 +564,10 @@ impl<'a> DAP<'a> {
 
         match self.mode {
             Some(DAPMode::SWD) => {
+                // Ensure SWD pins are in the right mode, in case they've been used as outputs
+                // by the SWJ_Pins command.
+                self.pins.swd_clk_spi();
+                self.pins.swd_tx();
                 self.swd.tx_sequence(seq);
             }
             Some(DAPMode::JTAG) => {
